7c3b0d304e15cd5203086f8af93a39f776f717e7
Luiz Fernandes de Oliveira <luizfernandesoliveiraoficial@gmail.com>
2024-02-09 20:45:15 -0300

    feat: adjust package

--- src/main/java/com/opensourcelibrary/storage/AutoConfigure.java
+++ src/main/java/com/opensourcelibrary/storage/AutoConfigure.java
@@ -11,10 +11,8 @@ import java.util.List;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 
-@Configuration
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 public class AutoConfigure {
 
